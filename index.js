322f40e11f354ade69795a3aaf49ba1f3e9d9fe3
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-24 01:26:13 +0900

    refactor(index.js): change variable 'env.port' -> 'env.PORT'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./src/index.route');
 const env = require('./config/env');
 
-app.listen(env.port, () => {
-  console.info(`server started on port ${env.port}`); // eslint-disable-line no-console
+app.listen(env.PORT, () => {
+  console.info(`server started on port ${env.PORT}`); // eslint-disable-line no-console
 });
 
 module.exports = app;
