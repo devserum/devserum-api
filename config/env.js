0d3a8a0cd6736462b0386981ad499c92745a331e
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-25 03:07:47 +0900

    feat(config/env): implement env parse logic

--- config/env.js
+++ config/env.js
@@ -1,7 +1,20 @@
 const result = require('dotenv').config();
 
-const env = result.parsed;
+const parsedEnv = result.parsed;
+const defaultSettings = {
+  PORT: 4000,
+};
 
-env.PORT = process.env.PORT || env.PORT || 4000;
+const mergeEnvironmentVariables = (processEnv, parsedEnv, defaultSettings) => {
+  Object.keys(parsedEnv).forEach((key) => {
+    processEnv[key] = processEnv[key] || parsedEnv[key];
+  });
+  Object.keys(defaultSettings).forEach((key) => {
+    processEnv[key] = processEnv[key] || defaultSettings[key];
+  });
+  return processEnv;
+};
+
+const env = mergeEnvironmentVariables(process.env, parsedEnv, defaultSettings);
 
 module.exports = env;
