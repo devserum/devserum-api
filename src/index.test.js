b66f1b569b670305c9e34edd193ccc1e59b44bc8
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-25 22:52:04 +0900

    style(index.test): change callback done -> async, await

--- src/index.test.js
+++ src/index.test.js
@@ -2,21 +2,19 @@ const request = require('./test-helper/request.test-helper');
 
 describe('# index test', () => {
   describe('## API test', () => {
-    test('# GET /', (done) => {
-      request
+    test('# GET /', async () => {
+      await request
         .get('/')
         .then((res) => {
           expect(res.statusCode).toEqual(200);
-          done();
         });
     });
     
-    test('# GET /health-check', (done) => {
-      request
+    test('# GET /health-check', async () => {
+      await request
         .get('/health-check')
         .then((res) => {
           expect(res.statusCode).toEqual(200);
-          done();
         });
     });
   });
