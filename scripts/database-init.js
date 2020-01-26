1245174bc31730a1dced525312535336d13dac52
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-27 02:32:10 +0900

    feat(scripts/database-init): implement database init script

---
+++ scripts/database-init.js
@@ -0,0 +1,28 @@
+const consola = require('consola');
+const { Client } = require('pg');
+
+const env = require('../config/env');
+
+const client = new Client({
+  host: env.PSQL_HOST,
+  port: env.PSQL_PORT,
+  user: env.PSQL_USERNAME,
+  password: env.PSQL_PASSWORD,
+  database: 'postgres',
+});
+
+const createDBQuery = `CREATE DATABASE ${env.PSQL_DATABASE}`;
+
+(async () => {
+  await client.connect()
+    .then(() => consola.info('INIT pg connect success'))
+    .catch((err) => consola.info(err));
+  
+  await client.query(createDBQuery)
+    .then(() => consola.info(`successfully create database "${env.PSQL_DATABASE}"`))
+    .catch(() => consola.info(`database "${env.PSQL_DATABASE}" already exist`))
+    .then(() => {
+    });
+  
+  client.end();
+})();
