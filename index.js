a0e5f9d3e870bb322318a99b49cc1f44a1fd5581
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-31 03:14:10 +0900

    refactor(indexsjs): config file name modify based on config file name change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/index.route');
-const env = require('./config/env');
+const env = require('./config/config');
 
 app.listen(env.PORT, () => {
   console.info(`server started on port ${env.PORT}`); // eslint-disable-line no-console
