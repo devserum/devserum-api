e4ee818122a487f0e6c58ca3b5f74ae6e0853dc3
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-11 01:48:00 +0900

    test(user.test): add create user API test

--- src/user/user.test.js
+++ src/user/user.test.js
@@ -1,9 +1,9 @@
-// const request = require('../test-helper/request.test-helper');
+const request = require('../test-helper/request.test-helper');
 const db = require('../../database');
 
 describe('# index test', () => {
-  // const getBaseURL = (URL) => `${URL}`;
-  // const baseURL = getBaseURL('/users');
+  const getBaseURL = (URL) => `${URL}`;
+  const baseURL = getBaseURL('/users');
   const getRandNumWithString = (prefix = 'test', max = 1000000) => `${prefix}${Math.floor(Math.random() * max)}}`;
   
   const mockUserBody = {
@@ -11,6 +11,17 @@ describe('# index test', () => {
     password: '1234567890',
     nickname: getRandNumWithString('test-nickname'),
   };
+  
+  describe('## users API test', () => {
+    test('# POST /users', async () => {
+      await request
+        .post(baseURL)
+        .send(mockUserBody)
+        .then((res) => {
+          expect(res.statusCode).toEqual(200);
+        });
+    });
+  });
   
   describe('## Model Test', () => {
     const modelName = 'User';
