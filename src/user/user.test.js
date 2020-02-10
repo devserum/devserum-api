49f2d89d31f55281548673ed905640c2a2046619
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-11 01:34:07 +0900

    test(user.test): implement user model test(create)

---
+++ src/user/user.test.js
@@ -0,0 +1,22 @@
+// const request = require('../test-helper/request.test-helper');
+const db = require('../../database');
+
+describe('# index test', () => {
+  // const getBaseURL = (URL) => `${URL}`;
+  // const baseURL = getBaseURL('/users');
+  const getRandNumWithString = (prefix = 'test', max = 1000) => `${prefix}${Math.floor(Math.random() * max)}}`;
+  
+  const mockUserBody = {
+    email: `${getRandNumWithString()}@gmail.com`,
+    password: '1234567890',
+    nickname: getRandNumWithString('test-nickname'),
+  };
+  
+  describe('## Model Test', () => {
+    const modelName = 'User';
+    test('create', async () => {
+      const result = await db[modelName].create(mockUserBody);
+      expect(result.email).toEqual(mockUserBody.email);
+    });
+  });
+});
