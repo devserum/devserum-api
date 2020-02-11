42d8142eddf814568088b8b2bbc11b2b2c60ea61
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-12 02:12:52 +0900

    test(user.test.js): implement '# Get /users/:userId' case

--- src/user/user.test.js
+++ src/user/user.test.js
@@ -4,26 +4,45 @@ const db = require('../../database');
 describe('# index test', () => {
   const getBaseURL = (URL) => `${URL}`;
   const baseURL = getBaseURL('/users');
-  const getRandNumWithString = (prefix = 'test', max = 1000000) => `${prefix}${Math.floor(Math.random() * max)}}`;
+  const getRandNumWithString = (prefix = 'test', max = 1000000) => `${prefix}${Math.floor(Math.random() * max)}`;
   
-  const mockUserBody = {
-    email: `${getRandNumWithString()}@gmail.com`,
-    password: '1234567890',
-    nickname: getRandNumWithString('test-nickname'),
-  };
   
   describe('## users API test', () => {
+    const mockUserBody = {
+      email: `${getRandNumWithString()}@gmail.com`,
+      password: '1234567890',
+      nickname: getRandNumWithString('test-nickname'),
+    };
+    
+    let createdUser;
     test('# POST /users', async () => {
       await request
         .post(baseURL)
         .send(mockUserBody)
         .then((res) => {
           expect(res.statusCode).toEqual(200);
+          expect(res.body.email).toEqual(mockUserBody.email);
+          createdUser = res.body;
+        });
+    });
+    
+    test('# GET /user/{userId}', async () => {
+      const url = `${baseURL}/${createdUser.id}`;
+      await request
+        .get(url)
+        .then((res) => {
+          expect(res.statusCode).toEqual(200);
+          expect(res.body.email).toEqual(createdUser.email);
         });
     });
   });
   
   describe('## Model Test', () => {
+    const mockUserBody = {
+      email: `${getRandNumWithString()}@gmail.com`,
+      password: '1234567890',
+      nickname: getRandNumWithString('test-nickname'),
+    };
     const modelName = 'User';
     test('create', async () => {
       const result = await db[modelName].create(mockUserBody);
