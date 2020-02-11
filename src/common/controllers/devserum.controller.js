0f07c4f793de0aa9d2004ef6e042c6cdd9ba0183
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-12 02:10:20 +0900

    feat(devserum.controller): implement 'get' method

--- src/common/controllers/devserum.controller.js
+++ src/common/controllers/devserum.controller.js
@@ -5,6 +5,10 @@ const ResponseController = require('./response.controller');
 const db = require('../../../database');
 
 class DevserumController extends ResponseController {
+  // static getDb() {
+  //   return db;
+  // }
+  
   static parseUrlLayer(req, depth = 1) {
     return req.originalUrl.split('/').slice(-depth);
   }
@@ -16,12 +20,28 @@ class DevserumController extends ResponseController {
   static async create(req, res, next, args) {
     const layers = DevserumController.parseUrlLayer(req);
     const targetModelName = DevserumController.capitalize(pluralize.singular(layers.pop()));
+    
     return super.asyncResponse(
       req,
       res,
       next,
       args,
       db[targetModelName].create(req.body),
+    );
+  }
+  
+  static async get(req, res, next, args) {
+    const layers = DevserumController.parseUrlLayer(req, 2);
+    
+    const targetModelId = layers.pop();
+    const targetModelName = DevserumController.capitalize(pluralize.singular(layers.pop()));
+    
+    return super.asyncResponse(
+      req,
+      res,
+      next,
+      args,
+      db[targetModelName].findByPk(targetModelId),
     );
   }
 }
