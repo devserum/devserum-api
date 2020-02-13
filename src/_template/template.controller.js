e77418155d5ac84a17755a08c61c1003efb111cf
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-14 01:42:33 +0900

    feat(template.controller): implement getList

--- src/_template/template.controller.js
+++ src/_template/template.controller.js
@@ -2,7 +2,11 @@ const DevserumController = require('../common/controllers/devserum.controller');
 
 class TemplateController extends DevserumController {
   static create(req, res, next) {
-    super.create(req, res, next);
+    return super.create(req, res, next);
+  }
+  
+  static getList(req, res, next) {
+    return super.getList(req, res, next);
   }
 }
 
