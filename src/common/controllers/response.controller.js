d1634d48e7859f174cd4fbf7c9a22a54aa882d5b
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-14 01:41:10 +0900

    feat(response.controller): implement rappingToStandardFormat, modelResponse

--- src/common/controllers/response.controller.js
+++ src/common/controllers/response.controller.js
@@ -1,9 +1,30 @@
 class ResponseController {
+  static rappingToStandardFormat(data) {
+    return {
+      data: null || data.rows,
+      nextLink: null,
+      prevLink: null,
+      metaInfos: {
+        effectedRows: null,
+        pageSize: null,
+        count: 0 || data.count,
+      },
+    };
+  }
+  
   static async asyncResponse(req, res, next, args, asyncFunc) {
     asyncFunc
       .then((result) => {
         res.send(result);
       });
+  }
+  
+  static async modelResponse(req, res, next, args, model) {
+    model
+      .then((result) => {
+        res.send(ResponseController.rappingToStandardFormat(result));
+      });
+    // .catch((e) => res.send(e));
   }
 }
 
