e42ad0e47b98fc4c53afac82aa9f8d65d0341063
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-02-19 02:04:58 +0900

    refactor(devserum.controller): update method naming change to interface

--- src/common/controllers/devserum.controller.js
+++ src/common/controllers/devserum.controller.js
@@ -62,25 +62,31 @@ class DevserumController extends ResponseController {
     );
   }
   
-  static async update(req, res, next, args) {
-    const layers = DevserumController.parseUrlLayer(req, 2);
-    
-    const targetModelId = layers.pop();
-    const targetModelName = DevserumController.capitalize(pluralize.singular(layers.pop()));
-    
-    return super.modelResponse(
-      req,
-      res,
-      next,
-      args,
-      db[targetModelName].update(
+  static get [DevserumInterface.actions.update]() {
+    return async (req) => {
+      const layers = DevserumController.parseUrlLayer(req, 2);
+      
+      const targetModelId = layers.pop();
+      const targetModelName = DevserumController.capitalize(pluralize.singular(layers.pop()));
+      
+      return db[targetModelName].update(
         req.body,
         {
           where: { id: targetModelId },
           returning: true,
         },
-      ),
-      'update',
+      );
+    };
+  }
+  
+  static get [DevserumInterface.actions.updateAndResponse]() {
+    return async (req, res, next, args) => super.modelResponse(
+      req,
+      res,
+      next,
+      args,
+      DevserumController[DevserumInterface.actions.update](req, res, next, args),
+      DevserumInterface.actions.update,
     );
   }
   
